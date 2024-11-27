e78d1db3821e943a257c7f1c5cd383e70790b70e
Sandis Miķelsons <sandza201@gmail.com>
2024-11-27 16:43:59 +0200

    Fixed welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-blog-layout>
     <div class="bg-[#f6f4ed] min-h-screen flex flex-col-reverse md:flex-row items-center relative px-6 py-12">
-        <img class="md:absolute md:right-0 md:top-1/2 md:bottom-1/2 md:-translate-y-1/2" src="/storage/images/7061503.svg" alt="">
-        <main class="mx-auto max-w-7xl h-full flex w-full">
+        <img class="md:absolute md:right-0 md:top-1/2 md:bottom-1/2 md:-translate-y-1/2" src="https://i.ibb.co/qkhfWQg/7061503.png" alt="">
+        <div class="mx-auto max-w-7xl h-full flex w-full">
             <div class="my-auto flex flex-col">
                 <h1 class="text-5xl md:text-[6rem] leading-none font-georgia">Home <br>
                     stories & ideas</h1>
@@ -9,6 +9,6 @@
                 <a href="{{ route('feed.index') }}"
                     class="px-7 py-3 bg-black text-white w-fit text-xl rounded-full mt-8">Start Reading</a>
             </div>
-        </main>
+        </div>
     </div>
 </x-blog-layout>
